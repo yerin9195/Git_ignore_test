17ad039698b291161509f1b061f833caae1d60ed
YERIN JUNG <156673142+yerin9195@users.noreply.github.com>
2024-05-10 11:31:24 +0900

    unchanged_assume.Assume_unchanged_8 클래스 완료함

--- Git_ignore_test/src/main/java/unchanged_assume/Assume_unchanged_8.java
+++ Git_ignore_test/src/main/java/unchanged_assume/Assume_unchanged_8.java
@@ -30,6 +30,8 @@ public class Assume_unchanged_8 {
 	*/
 		System.out.println("==== Assume_unchanged_8.java 파일입니다. ===="); 
 		System.out.println("==== 수정함 ===="); 
+		System.out.println("==== 2차 수정함 ===="); 
+		System.out.println("==== 마지막 완료 ===="); 
 
 	}
 
